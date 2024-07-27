f5c7e0904981fd2457b8451faaa697ecad9580c6
Rhzslya <Rhzslyaaa@gmail.com>
2024-07-27 22:39:47 +0700

    Modified Mongo Connections

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 export async function connect() {
   try {
     mongoose.connect(process.env.MONGODB_URI!);
--- src/lib/mongo/index.ts
+++ src/lib/mongo/index.ts
@@ -1,9 +1,11 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI!;
 const options = {};
 
-if (!uri) throw new Error("Please Add Your Mongo URI to .env.local");
+if (!uri) {
+  throw new Error("Please add your Mongo URI to .env.local");
+}
 
 let client = new MongoClient(uri, options);
 let clientPromise: Promise<MongoClient>;
@@ -12,7 +14,6 @@ if (process.env.NODE_ENV !== "production") {
   if (!global._mongoClientPromise) {
     global._mongoClientPromise = client.connect();
   }
-
   clientPromise = global._mongoClientPromise;
 } else {
   clientPromise = client.connect();
