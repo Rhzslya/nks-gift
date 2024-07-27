c64d2db45f871ded693e7c5e40b27c725bd3145b
Rhzslya <Rhzslyaaa@gmail.com>
2024-07-27 22:49:27 +0700

    Modified MongoDB

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -2,20 +2,28 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
+
 export async function connect() {
+  const uri = process.env.MONGODB_URI;
+  if (!uri) {
+    throw new Error("MONGODB_URI is not defined in environment variables");
+  }
+
   try {
-    mongoose.connect(process.env.MONGODB_URI!);
+    // Connect to MongoDB using Mongoose
+    await mongoose.connect(uri);
+
+    console.log("MongoDB connected successfully");
+
     const connection = mongoose.connection;
 
-    connection.on("connected", () => {
-      console.log("MongoDB connected successfully");
-    });
-
     connection.on("error", (err) => {
-      console.log("MongoDB connection error" + err);
-      process.exit();
+      console.error("MongoDB connection error:", err);
+      // Consider handling the error in a production environment
+      process.exit(1); // Exit with a failure code
     });
   } catch (error) {
-    console.log(error);
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1); // Exit with a failure code
   }
 }
--- src/lib/mongo/index.ts
+++ src/lib/mongo/index.ts
@@ -1,22 +1,23 @@
-import { MongoClient } from "mongodb";
+// //lib/mongo
+// import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI!;
-const options = {};
+// const uri = process.env.MONGODB_URI!;
+// const options = {};
 
-if (!uri) {
-  throw new Error("Please add your Mongo URI to .env.local");
-}
+// if (!uri) {
+//   throw new Error("Please add your Mongo URI to .env.local");
+// }
 
-let client = new MongoClient(uri, options);
-let clientPromise: Promise<MongoClient>;
+// let client = new MongoClient(uri, options);
+// let clientPromise: Promise<MongoClient>;
 
-if (process.env.NODE_ENV !== "production") {
-  if (!global._mongoClientPromise) {
-    global._mongoClientPromise = client.connect();
-  }
-  clientPromise = global._mongoClientPromise;
-} else {
-  clientPromise = client.connect();
-}
+// if (process.env.NODE_ENV !== "production") {
+//   if (!global._mongoClientPromise) {
+//     global._mongoClientPromise = client.connect();
+//   }
+//   clientPromise = global._mongoClientPromise;
+// } else {
+//   clientPromise = client.connect();
+// }
 
-export default clientPromise;
+// export default clientPromise;
