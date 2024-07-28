6627aba98cdf73cee9ca30b4a3b7b75d094236f6
Rhzslya <Rhzslyaaa@gmail.com>
2024-07-28 20:58:25 +0700

    separate components

---
+++ src/components/Button/SubmitButton/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+interface ButtonProps {
+  isLoading: boolean;
+  text: string;
+  type?: "button" | "submit" | "reset";
+  handleClick?: () => void;
+}
+const SubmitButton: React.FC<ButtonProps> = ({
+  isLoading,
+  text,
+  type = "button",
+  handleClick,
+}) => {
+  return (
+    <button
+      disabled={isLoading}
+      type={type}
+      onClick={handleClick}
+      className="w-full bg-sky-400 text-white px-2 py-[6px] text-[15px] rounded hover:bg-sky-300 duration-300 "
+    >
+      {isLoading ? "Loading..." : text}
+    </button>
+  );
+};
+
+export default SubmitButton;
---
+++ src/components/Navbar/NavLink/index.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import Link from "next/link";
+import { capitalizeFirst } from "@/utils/Capitalize";
+import Image from "next/image";
+type NavLinkProps = {
+  sectionsNav: string[];
+  isActiveLink: (path: string, linkPath: string) => boolean;
+  path: string;
+};
+
+const NavLink: React.FC<NavLinkProps> = ({
+  sectionsNav,
+  isActiveLink,
+  path,
+}) => {
+  return (
+    <div className="flex">
+      <div className="mr-8 flex items-center justify-center">
+        <Link href="/">
+          <Image
+            src="/icon.png"
+            alt="NKS Gift"
+            width={100}
+            height={100}
+            className="h-12 w-12 object-contain"
+          />
+        </Link>
+      </div>
+      <div className="flex items-center justify-center gap-4">
+        {sectionsNav?.map((item, index) => {
+          const linkPath = `/${item}`;
+          const isActive = isActiveLink(path, linkPath);
+
+          return (
+            <Link
+              href={linkPath}
+              key={index}
+              className={`link ${
+                isActive ? "text-sky-500" : ""
+              } hover:text-sky-300 duration-300 text-[15px] font-medium`}
+            >
+              {capitalizeFirst(item)}
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default NavLink;
