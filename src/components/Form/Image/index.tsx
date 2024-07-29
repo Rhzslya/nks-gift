2ccbafc9b4bab9a5f3961ccc669aa9f3f4a17169
Rhzslya <Rhzslyaaa@gmail.com>
2024-07-29 17:42:34 +0700

    delete default height

--- src/components/Form/Image/index.tsx
+++ src/components/Form/Image/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 const LogoForm = ({ src, alt }: { src: string; alt: string }) => {
-  return <Image src={src} alt={alt} width={100} height={100} />;
+  return <Image src={src} alt={alt} width={100} height={0} priority />;
 };
 
 export default LogoForm;
