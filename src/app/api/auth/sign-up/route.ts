12129dad76f7023ed9782c10fed43a5b436a5ce0
Rhzslya <Rhzslyaaa@gmail.com>
2024-07-29 17:47:46 +0700

    change name

--- src/app/api/auth/sign-up/route.ts
+++ src/app/api/auth/sign-up/route.ts
@@ -7,17 +7,14 @@ connect();
 
 export async function POST(request: NextRequest) {
   //Defines an Asyncronus Function
-  console.log("Received a Registration Request");
 
   try {
     //Get Data User From Body
     const reqBody = await request.json();
     const { username, email, password } = reqBody;
-    console.log("Request Data From Body", reqBody);
 
     // Check Email if Already in Database
     const user = await User.findOne({ email });
-    console.log("Checked Email in Database", user);
 
     // If User already in Database and User is Verified, return response 400
     if (user) {
