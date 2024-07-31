de550eb5afeeb441098dc90f04dd0206475f5775
Rhzslya <Rhzslyaaa@gmail.com>
2024-07-31 19:21:16 +0700

    feat : google login & middleware

---
+++ src/lib/services/sign-in-google/route.ts
@@ -0,0 +1,48 @@
+import User from "@/models/userModels";
+
+export async function handleGoogleSignIn(user: any, profile: any) {
+  const existingUser = await User.findOne({ email: user.email });
+
+  if (existingUser) {
+    // Update existing user
+    if (!existingUser.type?.includes("google")) {
+      existingUser.type.push("google"); // Update type to include Google
+      existingUser.googleId = user.id;
+      existingUser.profileImage = profile.picture;
+      existingUser.isVerified = profile.email_verified;
+      existingUser.username = existingUser.username || user.username;
+
+      console.log(existingUser);
+    } else {
+      // Set type to Google
+      existingUser.googleId = user.id;
+      existingUser.profileImage = profile.picture;
+      existingUser.isVerified = profile.email_verified;
+      existingUser.username = existingUser.username || user.username;
+
+      console.log(existingUser);
+    }
+    await existingUser.save();
+    user.id = existingUser._id.toString();
+    user.isAdmin = existingUser.isAdmin;
+    user.type = existingUser.type;
+  } else {
+    // Create new user
+    const newUser = new User({
+      email: user.email,
+      username: user.name,
+      googleId: user.id,
+      profileImage: profile.picture,
+      isAdmin: false,
+      isVerified: profile.email_verified,
+      type: ["google"], // Set type to Google
+    });
+
+    await newUser.save();
+    user.id = newUser._id.toString();
+    user.isAdmin = newUser.isAdmin;
+    user.type = newUser.type;
+  }
+
+  return user;
+}
---
+++ src/middleware.ts
@@ -0,0 +1,65 @@
+import { NextResponse, NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export async function middleware(req: NextRequest, res: NextResponse) {
+  const path = req.nextUrl.pathname;
+
+  // Define paths that are considered public (accessible without a token)
+
+  const isPublicPath =
+    path === "/" ||
+    path === "/sign-in" ||
+    path === "/sign-up" ||
+    path === "/verifyemail" ||
+    path === "/checkemail" ||
+    path === "/reset-password" ||
+    path === "/forgot-password";
+
+  const onlyAdmin = path === "/dashboard";
+
+  // Get the token from the request
+  const token = await getToken({
+    req,
+    secret: process.env.TOKEN_SECRET,
+  });
+
+  if (isPublicPath && token) {
+    // If trying to access a public path with a token, redirect to the home page
+    console.log(
+      "Redirecting to the home page because the path is public and a token was found"
+    );
+    return NextResponse.redirect(new URL("/", req.nextUrl));
+  }
+
+  // If trying to access a protected path without a token, redirect to the login page
+  if (!isPublicPath && !token) {
+    console.log(
+      "Redirecting to the login page because the path is protected and no token was found"
+    );
+    const url = new URL("/login", req.url);
+    url.searchParams.set("callbackUrl", encodeURI(req.url));
+    return NextResponse.redirect(url, { status: 303 });
+  }
+
+  if (onlyAdmin && token?.isAdmin !== true) {
+    return NextResponse.redirect(new URL("/", req.nextUrl));
+  }
+}
+
+// It specifies the paths for which this middleware should be executed.
+export const config = {
+  matcher: [
+    "/about",
+    "/profile",
+    "/sign-in",
+    "/sign-up",
+    "/verifyemail",
+    "/checkemail",
+    "/reset-password",
+    "/forgot-password",
+    "/dashboard/:path",
+    "/404",
+    "/not-found",
+    "/settings/:path*",
+  ],
+};
