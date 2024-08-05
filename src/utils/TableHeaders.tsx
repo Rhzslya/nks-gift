67e64e44b60208805d615cd01e228900681f2de0
Rhzslya <rhzslyaaa@gmail.com>
2024-08-05 20:11:55 +0700

    add table headers

---
+++ src/utils/TableHeaders.tsx
@@ -0,0 +1,9 @@
+export const tableHeaders = [
+  { name: "username", key: "username" },
+  { name: "email", key: "email" },
+  { name: "user id", key: "userId" },
+  { name: "type", key: "type" },
+  { name: "access level", key: "accessLevel" },
+  { name: "verified status", key: "verifiedStatus" },
+  { name: "date created", key: "dateCreated" },
+];
