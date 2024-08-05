ee54e0979b916324bdb07ed1d887729af575dca2
Rhzslya <rhzslyaaa@gmail.com>
2024-08-05 20:10:56 +0700

    add font

---
+++ src/utils/Font.tsx
@@ -0,0 +1,7 @@
+import { Bebas_Neue } from "next/font/google";
+
+export const bebasNeue = Bebas_Neue({
+  weight: ['400'], // Menentukan bobot yang digunakan
+  subsets: ['latin'], // Menentukan subset karakter yang digunakan
+  display: 'swap', // Opsi display untuk font
+});
