be4dddc77d3b2cacd6ff51779d11e8bd8db14939
Rhzslya <rhzslyaaa@gmail.com>
2024-08-20 21:23:15 +0700

    fix bug...

--- src/utils/ConvertNumber.tsx
+++ src/utils/ConvertNumber.tsx
@@ -1,5 +1,5 @@
 export const convertNumber = (phoneNumber: any) => {
-  // Jika nomor dimulai dengan '0', ganti dengan '62'
+  if (!phoneNumber) return ""; // Handle undefined or null case
   if (phoneNumber.startsWith("0")) {
     return "62" + phoneNumber.slice(1);
   }
